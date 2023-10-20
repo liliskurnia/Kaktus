3a786779df742fdd742cee26ad445f998c768a5f
Timotius Darmawan <timotius_darmawan@outlook.com>
2023-10-20 11:02:39 +0700

    renamed:    backend/src/db/migrations/20231018083400-create-jenis-sampah.js -> backend/src/db/migrations/20231018083200-create-jenis-sampah.js 	modified:   backend/src/db/migrations/20231018083308-create-sampah.js 	renamed:    backend/src/db/migrations/20231018084914-create-pickup-history.js -> backend/src/db/migrations/20231018083800-create-pickup-history.js 	renamed:    backend/src/db/migrations/20231018084942-create-order-history.js -> backend/src/db/migrations/20231018084000-create-order-history.js

--- backend/src/db/migrations/20231018083308-create-sampah.js
+++ backend/src/db/migrations/20231018083308-create-sampah.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'jenis_samphahs',
+          model: 'jenis_sampahs',
           key: 'id',
         },
       },
